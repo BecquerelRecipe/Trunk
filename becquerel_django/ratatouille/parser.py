60472f0ad4b49eb3c9a2b95b81e237624075f385
BecquerelRecipe <becquerelrecipe@gmail.com>
2017-04-26 22:04:44 +0100

    Scraper can get the html code from an url given after /scraper/ in the ratatouille url

--- becquerel_django/ratatouille/parser.py
+++ becquerel_django/ratatouille/parser.py
@@ -7,22 +7,13 @@ def ingredients_parser(stringToSearch):
     regexProduct = r"oeuf|beurre|sucre|amandes|feve"
     ##stringToSearch = "I want 500g cauliflower and 100g chocolate"
     if (re.search(regexProduct, stringToSearch) and re.search(regexQuantity, stringToSearch)):
-    #if (re.search(regexProduct, stringToSearch)):
-        # Indeed, the expression "([a-zA-Z]+) (\d+)" matches the date string
-
-        # If we want, we can use the MatchObject's start() and end() methods
-        # to retrieve where the pattern matches in the input string, and the
-        # group() method to get all the matches and captured groups.
         matchProduct = re.findall(regexProduct, stringToSearch)
         matchQuantity = re.findall(regexQuantity, stringToSearch)
         stringsBeforeProducts = re.split(regexProduct, stringToSearch)
-
-
         productQuantity = []
         print(matchProduct)
         print(matchQuantity)
         print(stringsBeforeProducts)
-
         for i in range(0, len(matchProduct)):
             resultSearch = re.search(regexQuantity, stringsBeforeProducts[i])
             if (resultSearch):
@@ -30,7 +21,6 @@ def ingredients_parser(stringToSearch):
             else:
                 productQuantity.append('')
         print(productQuantity)
-
     else:
         # If re.search() does not match, then None is returned
         print("The regex pattern does not match. :(")
